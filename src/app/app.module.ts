00addb5ff614a511b94f34d045f44cba49446f7f
kathe00 <89922196+kathe00@users.noreply.github.com>
2021-12-01 21:35:10 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ContenidoComponent } from './contenido/contenido.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    ContactoComponent,
+    ContenidoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/formato.ts
@@ -0,0 +1,12 @@
+//interfaz con el formato en que se guardan las noticias
+export interface Noticia{
+    id:number;
+    idTipo:number;
+    img1:string;
+    img2:string;
+    autor:string;
+    fecha:string;
+    titulo:string;
+    caption:string;
+    texto:string;
+}
